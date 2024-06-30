e4237f6afb8af24c3cb4694154ebc7b63ff48e3a
Vijay Khantwal <vkhantwal999@gmail.com>
2024-06-30 16:31:25 +0530

    HeapsDSA

--- Trees/Main.java
+++ Trees/Main.java
@@ -1,10 +1,11 @@
 package Trees;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 //        Scanner scanner = new Scanner(System.in);
 //        BinaryTree tree = new BinaryTree();
 //        tree.populate(scanner);
@@ -18,12 +19,23 @@ public class Main {
 //        System.out.println();
 //        tree.heightDisplay();
 
-        AVL_SelfBalancingTree tree = new AVL_SelfBalancingTree();
-        for(int i=1; i <= 100; i++) {
-            tree.insert(i);
-        }
-        tree.prettyDisplay();
-        System.out.println(tree.height());
+//        AVL_SelfBalancingTree tree = new AVL_SelfBalancingTree();
+//        for(int i=1; i <= 100; i++) {
+//            tree.insert(i);
+//        }
+//        tree.prettyDisplay();
+//        System.out.println(tree.height());
+
+        Heap<Integer> heap = new Heap<>();
+
+        heap.insert(35);
+        heap.insert(64);
+        heap.insert(27);
+        heap.insert(34);
+        heap.insert(10);
+
+        ArrayList<Integer> list = heap.heapSort();
+        System.out.println(list);
     }
 
 }
